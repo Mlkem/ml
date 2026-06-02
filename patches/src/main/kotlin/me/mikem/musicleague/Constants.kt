d09d1443e049cb8a195e98d81d06438bfa7dfbaa
Mlkem <31717098+Mlkem@users.noreply.github.com>
2026-06-02 20:12:29 -0300

    Update Constants.kt

--- patches/src/main/kotlin/me/mikem/musicleague/Constants.kt
+++ patches/src/main/kotlin/me/mikem/musicleague/Constants.kt
@@ -1,11 +1,13 @@
 package me.mikem.musicleague
 
-import app.morphe.patcher.data.AppTarget
-import app.morphe.patcher.data.Compatibility
+import app.morphe.patcher.patch.ApkFileType
+import app.morphe.patcher.patch.AppTarget
+import app.morphe.patcher.patch.Compatibility
 
 val MUSIC_LEAGUE = Compatibility(
     name = "Music League",
     packageName = "com.musicleague.app",
+    apkFileType = ApkFileType.APK,
     appIconColor = 0x9E00C4,
     targets = listOf(
         AppTarget(version = "1.4.9"),
