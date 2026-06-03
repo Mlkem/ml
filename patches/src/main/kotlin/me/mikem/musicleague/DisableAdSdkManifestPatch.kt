6c5ce43e9ae0ea66bc660099594263029678dd4b
Mlkem <31717098+Mlkem@users.noreply.github.com>
2026-06-02 21:50:46 -0300

    Update DisableAdSdkManifestPatch.kt

--- patches/src/main/kotlin/me/mikem/musicleague/DisableAdSdkManifestPatch.kt
+++ patches/src/main/kotlin/me/mikem/musicleague/DisableAdSdkManifestPatch.kt
@@ -58,10 +58,9 @@ val disableAdSdkManifestPatch = resourcePatch(
             "android.permission.ACCESS_ADSERVICES_CUSTOM_AUDIENCE",
         )
 
-        val adMetaValuesToSet = mapOf(
-            "com.google.android.gms.ads.APPLICATION_ID" to "ca-app-pub-0000000000000000~0000000000",
-            "com.google.android.gms.ads.DELAY_APP_MEASUREMENT_INIT" to "true",
-        )
+val adMetaValuesToSet = mapOf(
+    "com.google.android.gms.ads.DELAY_APP_MEASUREMENT_INIT" to "true",
+)
 
         val adMetaNamesToRemove = setOf(
             "com.google.android.gms.ads.flag.OPTIMIZE_INITIALIZATION",
